769ab9c41cff35a1e37a82a1502fd2d335ecc9bf
webgen1 <80384658+webgen1@users.noreply.github.com>
2024-06-06 21:37:33 +0300

    select

--- src/components/sections/Order/OrderField.tsx
+++ src/components/sections/Order/OrderField.tsx
@@ -1,4 +1,4 @@
-import { FC, ChangeEvent, useState } from "react";
+import { FC, ChangeEvent, useState, useRef, useEffect } from "react";
 import { useFormContext, Controller } from "react-hook-form";
 
 import DGDIcon from "@img/ui/Field/dgd.svg";
@@ -19,15 +19,15 @@ const selectData = [
 
 const OrderField: FC<IOrderFields> = ({ createField, removeField, index }) => {
   const [showSelect, setShowSelect] = useState<boolean>(false);
+  const selectRef = useRef<HTMLDivElement>(null);
 
   const { control, register, getValues, setValue } =
     useFormContext<IFormValues>();
-
   const fields = getValues(`fields`);
 
   const autoSize = (e: ChangeEvent<HTMLTextAreaElement>) => {
     e.target.style.height = "21px";
-    const scrollHeight = (e.target as HTMLTextAreaElement).scrollHeight;
+    const scrollHeight = e.target.scrollHeight;
     e.target.style.height = `${scrollHeight}px`;
 
     if (e.target.value.length > 3) {
@@ -37,6 +37,27 @@ const OrderField: FC<IOrderFields> = ({ createField, removeField, index }) => {
     }
   };
 
+  useEffect(() => {
+    if (showSelect) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showSelect]);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      selectRef.current &&
+      !selectRef.current.contains(event.target as Node)
+    ) {
+      setShowSelect(false);
+    }
+    setValue(`fields.${index}.route`, "");
+  };
+
   return (
     <div className={styles.fieldWrapper}>
       <div className={styles.dragButton}>
@@ -44,9 +65,7 @@ const OrderField: FC<IOrderFields> = ({ createField, removeField, index }) => {
       </div>
       <div className={styles.fullField}>
         <div
-          className={`${styles.field} ${
-            showSelect ? styles.fieldActive : null
-          }`}
+          className={`${styles.field} ${showSelect ? styles.fieldActive : ""}`}
         >
           <textarea
             placeholder={
@@ -61,7 +80,6 @@ const OrderField: FC<IOrderFields> = ({ createField, removeField, index }) => {
             data-isfield="true"
             {...register(`fields.${index}.route`)}
           />
-
           <div>
             <input
               type="number"
@@ -70,7 +88,6 @@ const OrderField: FC<IOrderFields> = ({ createField, removeField, index }) => {
               {...register(`fields.${index}.entrance`)}
             />
           </div>
-
           <div
             className={`${styles.createButton} ${
               index === 0 ? styles.show : styles.hide
@@ -79,7 +96,6 @@ const OrderField: FC<IOrderFields> = ({ createField, removeField, index }) => {
           >
             <CreateIcon />
           </div>
-
           <div
             className={`${styles.removeButton} ${
               index > 0 && index !== fields.length - 1 && fields.length > 2
@@ -90,7 +106,6 @@ const OrderField: FC<IOrderFields> = ({ createField, removeField, index }) => {
           >
             <RemoveIcon />
           </div>
-
           <div
             className={`${styles.placeButton} ${
               index === fields.length - 1 ? styles.show : styles.hide
@@ -101,21 +116,23 @@ const OrderField: FC<IOrderFields> = ({ createField, removeField, index }) => {
         </div>
       </div>
       {showSelect && (
-        <Controller
-          name={`fields.${index}.select`}
-          control={control}
-          render={({ field }) => (
-            <Select
-              list={selectData}
-              customClass="custom-class"
-              onChange={(address) => {
-                field.onChange(address);
-                setValue(`fields.${index}.route`, address);
-                setShowSelect(false);
-              }}
-            />
-          )}
-        />
+        <div ref={selectRef}>
+          <Controller
+            name={`fields.${index}.select`}
+            control={control}
+            render={({ field }) => (
+              <Select
+                list={selectData}
+                customClass="custom-class"
+                onChange={(address) => {
+                  field.onChange(address);
+                  setValue(`fields.${index}.route`, address);
+                  setShowSelect(false);
+                }}
+              />
+            )}
+          />
+        </div>
       )}
     </div>
   );
